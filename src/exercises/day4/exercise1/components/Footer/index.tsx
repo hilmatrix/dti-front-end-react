8fb514971042e990423762e62ad6cd96cf3d101d
Hilmatrix <hilmaul@gmail.com>
2024-09-30 13:23:52 +0700

    hyperlink fix footer

--- src/exercises/day4/exercise1/components/Footer/index.tsx
+++ src/exercises/day4/exercise1/components/Footer/index.tsx
@@ -10,10 +10,10 @@ const Index: FC = () => {
             <div className="footer-right">
                 <nav>
                     <ul>
-                        <li>LinkedIn</li>
-                        <li>Twitter</li>
-                        <li>Instagram</li>
-                        <li>Webflow</li>
+                        <li><a href="https://www.linkedin.com/in/hilmatrix/">LinkedIn</a></li>
+                        <li><a href="https://www.twitter.com">Twitter</a></li>
+                        <li><a href="https://www.instagram.com/hilmatrix">Instagram</a></li>
+                        <li><a>Webflow</a></li>
                     </ul>
                 </nav>
             </div>
