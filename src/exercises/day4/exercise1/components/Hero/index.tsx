02fd704cbcb7d7d4515e144fa4c2f30a05012b95
Hilmatrix <hilmaul@gmail.com>
2024-09-30 10:11:24 +0700

    day 4 exercise 1 add running text

--- src/exercises/day4/exercise1/components/Hero/index.tsx
+++ src/exercises/day4/exercise1/components/Hero/index.tsx
@@ -1,11 +1,28 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import HeroImage from "../../assets/hero.png";
 import "./style.css";
 
+
 const Index: FC = () => {
+    const [leftOffset, setLeftOffset] = useState(0);
+    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setLeftOffset(leftOffset => {return leftOffset < -1500 ? 1500 : leftOffset-15})
+        }, 30); 
+        return () => {
+          clearInterval(intervalId);
+        };
+      }, []);
+
+    const newLeftOffset = leftOffset + "px";
+
     return (
         <div className="hero">
             <img src={HeroImage}></img>
+            <div className="running" style={{left : newLeftOffset}}>
+                <span className="running-text">Webflow Developer - UI/UX Designer - Web Designer</span>
+            </div>
         </div>
     );
 }
