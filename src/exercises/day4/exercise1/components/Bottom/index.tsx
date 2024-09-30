ad291581dbc0feece38f062d2e5d558da1021127
Hilmatrix <hilmaul@gmail.com>
2024-09-30 10:28:40 +0700

    day 4 exercise 1 add bottom

--- src/exercises/day4/exercise1/components/Bottom/index.tsx
+++ src/exercises/day4/exercise1/components/Bottom/index.tsx
@@ -4,10 +4,10 @@ import "./style.css";
 const Index: React.FC = () => {
   return (
     <div className='bottom'>
-        <div className="footer-left-up">
+        <div className="bottom-left">
                     <span className="title">Have something in mind ?<br></br> Let's build it together</span>
                 </div>
-                <div className="footer-right-up">
+                <div className="bottom-right">
                     <button>Get In touch</button>
                 </div>
     </div>
--- src/exercises/day4/exercise1/index.tsx
+++ src/exercises/day4/exercise1/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './components/About/index';
+import Bottom from './components/Bottom/index';
 import Footer from './components/Footer/index';
 import Header from './components/Header/index';
 import Hero from './components/Hero/index';
@@ -38,6 +39,7 @@ const Index: React.FC = () => {
       <WorkStrip images={arrayOfImages1}/>
       <WorkStrip images={arrayOfImages2}/>
       <Testimonials/>
+      <Bottom></Bottom>
       <Footer/>
     </>
   );
